473f32cf4aa41088201f9e0335f5509c8c74081c
Aslan Goldenhour <aslan@mimicry.finance>
2023-09-05 21:45:31 -0400

    initial commit of skeleton - testing husky

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+// See: https://eslint.org/docs/latest/use/configure/
+
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "plugins": ["@typescript-eslint"],
+    "extends": ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import dotenv from 'dotenv';
+
+//For env File
+dotenv.config();
+    
+console.log("foobar!")
